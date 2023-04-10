951fe48b73ccee392745703701818ed299801f7b
hiroki-yama-1118 <norishiro1118@gmail.com>
2023-04-10 13:19:47 +0900

    Start Server

--- main.go
+++ main.go
@@ -1,59 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"todo_app/app/controllers"
 	"todo_app/app/models"
 )
 
 func main() {
-// fmt.Println(models.Db)
+fmt.Println(models.Db)
 
-// u := &models.User{}
-// u.Name="test2"
-// u.Email="test2@example.com"
-// u.PassWord="testtest"
-// fmt.Println(u)
-
-// u.CreateUser()
-
-// u,_:=models.GetUser(1)
-
-// fmt.Println(u)
-
-// u.Name="Test2"
-// u.Email="Test2@example.com"
-// u.UpdateUser()
-// u,_=models.GetUser(1)
-// fmt.Println(u)
-
-// u.DeleteUser()
-// u,_=models.GetUser(1)
-// fmt.Println(u)
-
-// user,_:=models.GetUser(2)
-// user.CreateTodo("First Todo")
-
-// t,_:=models.GetTodo(1)
-// fmt.Println(t)
-
-// user,_:=models.GetUser(3)
-// user.CreateTodo("Third Todo")
-
-// todos,_:=models.GetTodos()
-// for _,v:= range todos{
-// 	fmt.Println(v)
-// }
-
-// user2,_:=models.GetUser(3)
-// todos,_:=user2.GetTodosByUser()
-// for _,v:=range todos{
-// 	fmt.Println(v)
-// }
-
-// t,_:=models.GetTodo(1)
-// t.Content="Update Todo"
-// t.UpdateTodo()
-
-t,_:=models.GetTodo(3)
-t.DeleteTodo()
+err := controllers.StartMainServer()
+if err != nil {
+		log.Fatalln(err)
+}
 
 }
