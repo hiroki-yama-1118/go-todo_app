070b7758bf65b79a1d4967c45b2cbbec8f4c287f
hiroki-yama-1118 <norishiro1118@gmail.com>
2023-04-10 11:40:08 +0900

    updateTodo

--- app/models/todos.go
+++ app/models/todos.go
@@ -84,3 +84,13 @@ func (u *User) GetTodosByUser()(todos []Todo,err error){
 	rows.Close()
 	return todos,err
 }
+
+func (t *Todo)UdateTodo()error{
+	cmd:=`update todos set content = ?,user_id = ? where id = ?`
+	_,err =Db.Exec(cmd,t.Content,t.UserID,t.ID)
+	if err !=nil{
+		log.Fatalln(err)
+	}
+	return err
+
+}
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"todo_app/app/models"
 )
 
@@ -44,10 +43,14 @@ func main() {
 // 	fmt.Println(v)
 // }
 
-user2,_:=models.GetUser(3)
-todos,_:=user2.GetTodosByUser()
-for _,v:=range todos{
-	fmt.Println(v)
-}
+// user2,_:=models.GetUser(3)
+// todos,_:=user2.GetTodosByUser()
+// for _,v:=range todos{
+// 	fmt.Println(v)
+// }
+
+t,_:=models.GetTodo(1)
+t.Content="Update Todo"
+t.UdateTodo()
 
 }
