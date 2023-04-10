d40e025272c6bf5c3ea4ecd220c2d890cdf05ebc
hiroki-yama-1118 <norishiro1118@gmail.com>
2023-04-10 15:14:55 +0900

    ログイン機能追加

--- main.go
+++ main.go
@@ -15,4 +15,15 @@ if err != nil {
 		log.Fatalln(err)
 }
 
+// user,_ :=models.GetUserByEmail("test@test.com")
+// fmt.Println(user)
+
+// session,err :=user.CreateSession()
+// if err!=nil{
+// 	log.Println(err)
+// }
+// fmt.Println(session)
+
+// valid,_ := session.CheckSession()
+// fmt.Println(valid)
 }
